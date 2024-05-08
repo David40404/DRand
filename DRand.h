14eb903960cc4c10d28d6e07047a83553a308f7e
David40404 <88509902+David40404@users.noreply.github.com>
2024-05-08 17:52:49 +0300

    Update DRand.h

--- DRand.h
+++ DRand.h
@@ -3,6 +3,7 @@
 #include <time.h>
 
 namespace DRand {
+  // recommended
   unsigned int DRand32() {
       static unsigned long long seed = (unsigned long long)time(NULL);
   
@@ -13,7 +14,34 @@ namespace DRand {
   
       return static_cast <unsigned int> (seed % UINT32_MAX);
   }
+  
+  // not tested
+  unsigned int DRand32x() {
+      static unsigned long long seed = static_cast <unsigned long long>(time(NULL));
+      static unsigned long long special_seed = 0;
 
+      static const unsigned long long a = 43252145235;
+      static const unsigned long long b = 134163156627;
+
+      special_seed ^= seed + b;
+      seed ^= a * (seed >> 32) + special_seed;
+
+      return static_cast <unsigned int> (seed % UINT32_MAX);
+  }
+
+  // not tested
+  unsigned int DRand32o() {
+      static unsigned long long seed = static_cast <unsigned long long>(time(NULL));
+
+      static const unsigned long long a = 43252145235;
+      static const unsigned long long b = 134163156627;
+
+      seed ^= (seed >> 2) * seed + a;
+
+      return static_cast <unsigned int> (seed % UINT16_MAX);
+  }
+
+  // recommended
   unsigned short DRand16() {
       static unsigned int seed = (unsigned int)time(NULL);
 
@@ -25,6 +53,7 @@ namespace DRand {
       return static_cast <unsigned short> (seed % UINT16_MAX);
   }
 
+  // recommended
   unsigned short DRand16x() {
       static unsigned int seed = (unsigned int)time(NULL);
       static unsigned int special_seed = 0;
@@ -35,6 +64,16 @@ namespace DRand {
       special_seed ^= seed + b;
       seed ^= a * (seed >> 16) + special_seed;
   
+      return static_cast <unsigned short> (seed % UINT16_MAX);
+  }
+
+  // recommended
+  unsigned short DRand16o() {
+      static unsigned int seed = static_cast <unsigned short> (time(NULL));
+      static const unsigned int a = 2932355;
+           
+      seed ^= (seed >> 2) * seed + a;
+
       return static_cast <unsigned short> (seed % UINT16_MAX);
   }
 }
